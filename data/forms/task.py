6ffd81a287ffc7675b397f1bf79617bd4442d709
vlad1xxx <vlad1x2807@gmail.com>
2024-04-28 11:07:17 +0300

    update

--- data/forms/task.py
+++ data/forms/task.py
@@ -1,7 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField
-from wtforms import BooleanField, SubmitField, RadioField, StringField
+from wtforms import BooleanField, SubmitField, RadioField, StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class TaskForm(FlaskForm):
     submit = SubmitField('ответить')
+
+
+class UpdateTaskForm(FlaskForm):
+    name_task = StringField('Название задания', validators=[DataRequired()])
+    text_task = TextAreaField('Текст задания')
+
+    text_task1 = StringField('Вариант 1', validators=[DataRequired()])
+    radio_task1 = RadioField('Правильный вариант')
+
+    text_task2 = StringField('Вариант 2', validators=[DataRequired()])
+    radio_task2 = RadioField('Правильный вариант')
+
+    text_task3 = StringField('Вариант 3', validators=[DataRequired()])
+    radio_task3 = RadioField('Правильный вариант')
+
+    text_task4 = StringField('Вариант 4', validators=[DataRequired()])
+    radio_task4 = RadioField('Правильный вариант')
+
+    submit = SubmitField('Сохранить')
+
